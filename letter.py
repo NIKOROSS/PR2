997f873f0b535baaff0d2df6de3cbc2930b4a7fe
NIKOROSS <isip_a.m.sorokin@mpt.ru>
2022-05-20 14:51:59 +0300

    letter

---
+++ letter.py
@@ -0,0 +1,6 @@
+def stroka(s):
+    clear()
+    print("Строка - " + str(s))
+    print("Длина введённой строки = " + str(len(s)))
+    print("Число пробелов = " + str(s.count(' ')))
+    print("Число запятых = " + str(s.count(',')))
